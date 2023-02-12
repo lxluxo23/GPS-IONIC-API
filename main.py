c78e53721aa5741a497b17b462a72ee8ab7fcf42
Luis cespedes <lxluxo23@gmail.com>
2023-02-12 20:51:00 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route("/api/cordenadas", methods=["POST"])
+def recibir_datos():
+    datos = request.get_json()
+    print(datos)
+    return "Datos recibidos correctamente"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
